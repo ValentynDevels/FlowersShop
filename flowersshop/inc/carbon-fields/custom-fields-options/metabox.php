eb7f38a2a11f3c03aef2dc7f08d8ac376463e4ef
Valentyn31 <polishchykvalentyn2017@gmail.com>
2020-11-17 18:30:01 +0200

    al in one 1.0.3

--- flowersshop/inc/carbon-fields/custom-fields-options/metabox.php
+++
@@ -1,9 +0,0 @@
-<!-- Complex field -->
-<?php 
-$slides = carbon_get_post_meta( get_the_ID(), 'crb_slides' );
-if ( $slides ) {
-    foreach ( $slides as $slide ) {
-        echo $slide['image'];
-    }
-}
-?>
--- flowersshop/page-my-account.php
+++
@@ -1,9 +0,0 @@
-<?php
-  get_header();
-
-  the_content();
-
-  get_sidebar();
-
-  get_footer();
-?>
